087a70b195ef4551ca7b2096ce8ffdf477127de1
techdevscode <steve@techdevs.io>
2020-07-23 20:06:07 +0100

    a

--- MCP/MCP.ino
+++ MCP/MCP.ino
@@ -1,5 +1,6 @@
 #include <RotaryEncoder.h>
 #include <Joystick.h>
+#include <Keyboard.h>
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_MULTI_AXIS,
   2, 0,                  // Button Count, Hat Switch Count
@@ -19,40 +20,30 @@ void setup()
   //Serial.begin(57600);
   // Initialize Joystick Library
   Joystick.begin();
+  //Keyboard.begin();
 } 
 
 
 // Read the current position of the encoder and print out when changed.
 void loop()
 {
-   
-  
-  
   static int e1 = 0;
   encoder.tick();
   int e1New = encoder.getPosition();
   if (e1 != e1New) {
     if(e1New < e1) { 
-      Joystick.setButton(0, 1);
-      btn0Press = millis();
-      //Serial.println("Down");
+      Keyboard.print("Down");
     }
-    if(e1New > e1) {
-      Joystick.setButton(1, 1);
-      btn1Press = millis();
-      //Serial.println("Up"); 
+    if(e1New > e1) { 
+      Keyboard.print("Up");
     }
+
+    delay(1);
+
+    //Joystick.setButton(0, 0);
+    //Joystick.setButton(1, 0);
 
     // Set back to unpressed 
     e1 = e1New;
   }
-
-  
-    if((millis() - btn0Press) >= 50) {
-      //Serial.println("Btn 1 off");
-      Joystick.setButton(0,0);
-    }
-    if((millis() - btn1Press) >= 50) {
-      Joystick.setButton(1,0);
-    }
 }
